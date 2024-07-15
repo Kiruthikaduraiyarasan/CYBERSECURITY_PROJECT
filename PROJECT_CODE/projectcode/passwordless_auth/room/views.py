ee0ff41cb6af45fda6c800f101ce49fe64bbcdc1
Kiruthikaduraiyarasan <127034315+Kiruthikaduraiyarasan@users.noreply.github.com>
2024-07-15 12:10:24 +0530

    Add files via upload

---
+++ PROJECT_CODE/projectcode/passwordless_auth/room/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from .models import Room,Message
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms': rooms})
+@login_required
+def room(request, slug):
+    messages=[]
+    try:
+        room = Room.objects.get(slug=slug)
+        messages = Message.objects.filter(room=room)[0:25]
+    except:
+        room = None
+
+    return render(request, 'room/room.html', {'room': room, 'messages':messages})
